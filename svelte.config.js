1ec27edd1c98295b42c401a5538b79d52b401ea8
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2021-05-17 14:55:00 -0700

    updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import netlify from '@sveltejs/adapter-netlify';
 
+import pkg from './package.json';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -11,6 +12,11 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: netlify(),
+		vite: {
+			ssr: {
+			  noExternal: Object.keys(pkg.dependencies || {}),
+			},
+		  },
 	}
 };
 
