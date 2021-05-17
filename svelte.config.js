31d58aa9a7c6cdd8035eaff80e80a49d1e5a2214
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2021-05-17 16:03:03 -0700

    more build stuff

--- svelte.config.js
+++ svelte.config.js
@@ -10,12 +10,13 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter(),
+		prerender: {
+			crawl: true,
+			enabled: true,
+			force: true,
+			pages: ['*'],
+		},
 	}
 };
 
