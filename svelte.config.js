c677561dc69fb090f17aa6583eb721cd5c68fff5
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2021-05-17 15:34:15 -0700

    fiddling with build

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,21 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
-const pkg = require('./package.json');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	
+
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify(),
-		vite: {
-			ssr: {
-			  noExternal: Object.keys(pkg.dependencies || {}),
-			},
-		  },
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
