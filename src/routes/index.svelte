ba6c461e9697bcad0adb2c5db04f3d605b9671a3
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2021-05-20 09:10:02 -0700

    refactored how its checking for body parts so its reusable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,13 +8,30 @@
 	let browserSupported = true;
 	let video: HTMLVideoElement;
 	let detector: any;
-	let leftElbowCoord = { x: null, y: null };
-	let rightHandCoord = { x: null, y: null };
-    let ready = false;
-    
-    let rightHandExists = false;
-    let leftElbowExists = false;
-	$: haveBoth = rightHandExists && leftElbowExists;
+	let ready = false;
+
+    interface Keypoint {
+		name: string;
+		score: number;
+		x: number;
+		y: number;
+	}
+	interface BodyPartStats {
+		partOneVisible: boolean;
+		partTwoVisible: boolean;
+		isTouching: boolean;
+	}
+
+	let rightHandTouchingElbow: BodyPartStats = {
+		partOneVisible: false,
+		partTwoVisible: false,
+		isTouching: false
+	};
+    let leftHandTouchingRightElbow: BodyPartStats = {
+		partOneVisible: false,
+		partTwoVisible: false,
+		isTouching: false
+	};
 
 	onMount(() => {
 		if (canUseCamera()) {
@@ -25,79 +42,88 @@
 	});
 
 	function canUseCamera() {
-        const getUserMediaCheck = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+		const getUserMediaCheck = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 		return getUserMediaCheck;
 	}
 	async function getVideoStream() {
 		video = document.querySelector('#video');
-		await navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+		await navigator.mediaDevices.getUserMedia({ video: { aspectRatio: 1920/1080 } }).then((stream) => {
 			video.srcObject = stream;
-            video.onloadeddata = () => {
-                setupTensorFlow();
-            };
+			video.onloadeddata = () => {
+				setupTensorFlow();
+			};
 		});
 	}
 	async function setupTensorFlow() {
 		// Create a detector.
 		const model = poseDetection.SupportedModels.MoveNet;
 		detector = await poseDetection.createDetector(model);
-        ready = true;
+		ready = true;
 		getPoses();
 	}
 
 	async function getPoses() {
 		const poses = await detector.estimatePoses(video);
 		const { keypoints } = poses[0];
-        
-		const rightHand = keypoints.find((pose) => pose.name === 'right_wrist');
-		if (rightHand.score > 0.1) {
-			rightHandCoord = { x: rightHand.x, y: rightHand.y };
-			rightHandExists = true;
-		} else {
-			rightHandCoord = { x: null, y: null };
-			rightHandExists = false;
-		}
 
-		const leftElbow = keypoints.find((pose) => pose.name === 'left_elbow');
-		if (leftElbow.score > 0.1) {
-			leftElbowCoord = { x: leftElbow.x, y: leftElbow.y };
-			leftElbowExists = true;
-		} else {
-			leftElbowCoord = { x: null, y: null };
-            leftElbowExists = false;
-		}
+		rightHandTouchingElbow = isBodyPartCloseToEachOther(keypoints, 'right_wrist', 'left_elbow');
+        leftHandTouchingRightElbow = isBodyPartCloseToEachOther(keypoints, 'left_wrist', 'right_elbow');
 		requestAnimationFrame(getPoses);
 	}
 
-    $: xDiff = Math.abs(rightHandCoord.x - leftElbowCoord.x);
-    $: yDiff = Math.abs(rightHandCoord.y - leftElbowCoord.y);
-	$: prettyDamnClose = haveBoth && xDiff < 100 && yDiff < 100;
+	function isBodyPartCloseToEachOther(
+		keypoints: Keypoint[],
+		partOne: string,
+		partTwo: string
+	): BodyPartStats {
+		const partOneKeypoint = keypoints.find((pose) => pose.name === partOne);
+		const partTwoKeypoint = keypoints.find((pose) => pose.name === partTwo);
+
+		const xDiff = Math.abs(partOneKeypoint.x - partTwoKeypoint.x);
+		const yDiff = Math.abs(partOneKeypoint.y - partTwoKeypoint.y);
+        
+		return {
+			partOneVisible: partOneKeypoint.score > 0.1,
+			partTwoVisible: partTwoKeypoint.score > 0.1,
+			isTouching: xDiff < 100 && yDiff < 100
+		};
+	}
+    
 </script>
 
 {#if browserSupported}
-	<h1>Touch your left elbow</h1>
+    <main>
+        <h1>Touch your left elbow</h1>
+        <p>
+            <a href="https://github.com/czaas/touch-your-elbow" target="_blank">Source code on github</a>
+        </p>
 
-    <p><a href="https://github.com/czaas/touch-your-elbow" target="_blank">Source code on github</a></p>
+        <video autoplay id="video" />
 
-	<video autoplay id="video" />
-
-    {#if ready}
-        {#if !rightHandExists && !leftElbowExists}
-            <p>We cannot see your left elbow or your right hand</p>
-        {:else if rightHandExists && !leftElbowExists}
-            <p>We can see your right hand but not your left elbow</p>
-        {:else if !rightHandExists && leftElbowExists}
-            <p>We can see your left elbow but not your right hand</p>
-        {:else if !prettyDamnClose}
-            <p>Now touch</p>
+        {#if ready}
+            {#if leftHandTouchingRightElbow.isTouching && !rightHandTouchingElbow.isTouching}
+                <p>Wrong elbow!</p>
+            {:else}
+                {#if !rightHandTouchingElbow.partOneVisible && !rightHandTouchingElbow.partTwoVisible}
+                    <p>We cannot see your left elbow or your right hand</p>
+                {:else if rightHandTouchingElbow.partOneVisible && !rightHandTouchingElbow.partTwoVisible}
+                    <p>We can see your right hand but not your left elbow</p>
+                {:else if !rightHandTouchingElbow.partOneVisible && rightHandTouchingElbow.partTwoVisible}
+                    <p>We can see your left elbow but not your right hand</p>
+                {:else if !rightHandTouchingElbow.isTouching}
+                    <p>Now touch</p>
+                {:else}
+					{#if leftHandTouchingRightElbow.isTouching && rightHandTouchingElbow.isTouching}
+						<p>You're touching your both elbows, but that's okay!</p>
+					{:else}
+                    	<p>Yay! You're touching your left elbow</p>
+					{/if}
+                {/if}
+            {/if}
+        {:else}
+            <p>Loading...</p>
         {/if}
-
-        {#if prettyDamnClose}
-            <p>Yay! You're touching your elbow (more or less)</p>
-        {/if}
-    {:else}
-        <p>Loading...</p>
-    {/if}
+    </main>
 {:else}
 	<h1>Your Browser is not supported</h1>
 {/if}
@@ -105,11 +131,13 @@
 <style lang="scss">
 	:global(body) {
 		text-align: center;
-        font-size: 20px;
+		font-size: 20px;
 	}
 	video {
 		// turn video around because it's like a mirror
 		transform: rotateY(180deg);
-		margin: 0 auto;
+        width: 760px;
+        max-height: 80vh;
+        max-width: 100%;
 	}
 </style>
